9a76e3b376a767f24646ac8f59f5f24f7a88693c
faruk kartal <farukkartal@faruk-MacBook-Air.local>
2026-02-01 04:59:13 +0300

    kütüphane ve makefile düzeltmesi

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: fakartal <fakartal@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 15:14:17 by fakartal          #+#    #+#             */
-/*   Updated: 2026/01/09 20:37:57 by fakartal         ###   ########.fr       */
+/*   Updated: 2026/01/31 20:12:45 by fakartal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ char	*ft_strrchr(const char *s, int c)
 	{
 		i++;
 	}
-	while (i >= 0)
+	while (i > 0)
 	{
 		if (s[i] == (char)c)
 			return ((char *)s + i);
 		i--;
 	}
-	return (0);
+	if (s[0] == (char)c)
+		return ((char *)(s + 0));
+	return (NULL);
 }
